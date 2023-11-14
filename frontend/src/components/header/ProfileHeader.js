40d35be037f5ae668e7246cca2936ee8ce7aee2f
Bhanu Pratap Singh Rathore <bhanupratapsingh3011@gmail.com>
2023-11-15 00:29:26 +0530

    Profile design and popup
    just looking like a wow

--- frontend/src/components/header/ProfileHeader.js
+++ frontend/src/components/header/ProfileHeader.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { images } from '../../images';
 import { useNavigate } from 'react-router-dom';
 import "./profileHeader.scss";
+import '../../modal/profilePopupModal/profilePopup.scss';
 
 const ProfileHeader = () => {
   // Initialize the navigate function from react-router-dom
@@ -16,7 +17,14 @@ const ProfileHeader = () => {
     setOpen(!open);
   }
 
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
+
   return (
+    
     <div className='profileHeader'>
       {/* Navigation bar */}
       <nav className="navbar container">
@@ -31,7 +39,25 @@ const ProfileHeader = () => {
           <li>Contact</li>
           <li>Details</li>
           <li>Complain Box</li>
-          <li id='pfp2'> <img src={images.people_first} alt='' /> </li>
+          <div className="profile-popup-container" onClick={togglePopup}>
+          <li className="profile-button"> <img src={images.people_first} alt='profile'/> </li>
+          {isPopupOpen && (
+            <div className="profile-popup">
+                <div className='popupHead'>
+                  <h3 className='profileName'><b>Aamir</b></h3>
+                  <br />
+                  <h3 className='profileRegNo'><b>20214197</b></h3>
+                </div>
+                <hr />
+                <div className='popupBottom'>
+                  <h4 className='updatePassword'><b>Change Password</b></h4>
+                  <br />
+                  <h4 className='logOut'><b>Log Out</b></h4>
+                </div>
+                
+            </div>
+          )}
+        </div>
         </ul>
 
         {/* Hamburger menu icon for mobile */}
